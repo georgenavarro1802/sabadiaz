ad9993cef44377cfc2057d96afcb8d0b3391fb17
georgenavarro1802 <gnavarrog1802@gmail.com>
2019-11-13 22:14:34 -0500

    first commit

---
+++ app/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+
+
+def index(request):
+    data = {
+        'title': 'Sabadiaz Jewelry - Welcome',
+        'option': 'index'
+    }
+    return render(request, 'index.html', data)
+
+
+def product_details(request):
+    data = {
+        'title': 'Sabadiaz Jewelry - Product Details',
+        'option': 'product'
+    }
+    return render(request, 'product_details.html', data)
+
+
+def shop(request):
+    data = {
+        'title': 'Sabadiaz Jewelry - Shop',
+        'option': 'shop'
+    }
+    return render(request, 'shop.html', data)
+
+
+def about(request):
+    data = {
+        'title': 'Sabadiaz Jewelry - About Us',
+        'option': 'about'
+    }
+    return render(request, 'about.html', data)
+
+
+def contact(request):
+    data = {
+        'title': 'Sabadiaz Jewelry - Contact Us',
+        'option': 'contact'
+    }
+    return render(request, 'contact.html', data)
+
+
+def wishlist(request):
+    data = {
+        'title': 'Sabadiaz Jewelry - WishList',
+        'option': 'wishlist'
+    }
+    return render(request, 'wishlist.html', data)
---
+++ sabadiaz/urls.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.urls import path
+from app import views
+
+
+urlpatterns = [
+
+    # admin
+    path('admin/', admin.site.urls),
+
+    # index
+    path('', views.index, name='index'),
+
+    # Shop
+    path('shop', views.shop, name='shop'),
+
+    # About Us
+    path('about', views.about, name='about'),
+
+    # Contact Us
+    path('contact', views.contact, name='contact'),
+
+    # Wishlist
+    path('wishlist', views.wishlist, name='wishlist'),
+
+    # Product Details
+    path('product/<int:product_id>/details', views.product_details, name='product_details'),
+
+]
