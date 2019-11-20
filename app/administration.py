a8ecb67360a13f745830ae562e77169f012a536d
georgenavarro1802 <gnavarrog1802@gmail.com>
2019-11-20 13:05:02 -0500

    Working in the Administration interface

--- app/administration.py
+++ app/administration.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 def products(request):
     data = {
         'title': 'Sabadiaz Jewelry Admin - All Products',
-        'option': 'products',
-        'user': request.user
+        'option': 'admin_products',
+        'user': request.user,
+        'lista': [x for x in range(20)]
     }
     return render(request, 'administration/products.html', data)
