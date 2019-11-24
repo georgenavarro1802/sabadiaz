27051794e66eb889cc087ecd902064c0016d0cc4
georgenavarro1802 <gnavarrog1802@gmail.com>
2019-11-24 17:51:47 -0500

    Optimizations and syling changes

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
@@ -8,73 +10,74 @@ from app.helpers import ok_json, bad_json
 from app.models import Product, Slide, Category, CompanyData, Testimonial
 
 
+def addUserData(request, data):
+    data['user'] = request.user
+    data['company'] = CompanyData.objects.first()
+
+
 # Website Views
 def index(request):
-    products = Product.objects.all()
-    data = {
-        'title': 'Sabadiaz Jewelry - Welcome',
-        'option': 'index',
-        'user': request.user,
-        'slides': Slide.objects.all(),
-        'testimonials': Testimonial.objects.all(),
-        'categories': Category.objects.filter(product__isnull=False).distinct(),
-        'featured_products': products.filter(is_featured=True)[:10],
-        'bestseller_products': products.filter(is_bestseller=True)[:8],
-        'onsale_products': products.filter(is_onsale=True)[:8],
-        'company': CompanyData.objects.first(),
-    }
+    data = {'title': 'Sabadiaz Jewelry - Welcome'}
+    addUserData(request, data)
+
+    data['option'] = 'index'
+    data['slides'] = Slide.objects.all()
+    data['testimonials'] = Testimonial.objects.all()
+    data['categories'] = Category.objects.filter(product__isnull=False).distinct()
+    data['featured_products'] = Product.objects.filter(is_featured=True)[:10]
+    data['bestseller_products'] = Product.objects.filter(is_bestseller=True)[:8]
+    data['onsale_products'] = Product.objects.filter(is_onsale=True)[:8]
     return render(request, 'site/home.html', data)
 
 
-def shop(request):
-    data = {
-        'title': 'Sabadiaz Jewelry - Shop',
-        'option': 'shop'
-    }
-    return render(request, 'site/shop.html', data)
-
-
 def about(request):
-    data = {
-        'title': 'Sabadiaz Jewelry - About Us',
-        'option': 'about'
-    }
+    data = {'title': 'Sabadiaz Jewelry - About Us'}
+    addUserData(request, data)
+    data['option'] = 'about'
+    data['current_page'] = 'About Us'
+    data['testimonials'] = Testimonial.objects.all()
     return render(request, 'site/about.html', data)
 
 
+def shop(request):
+    data = {'title': 'Sabadiaz Jewelry - Shop'}
+    addUserData(request, data)
+    data['option'] = 'shop'
+    data['current_page'] = 'Shop'
+    return render(request, 'site/shop.html', data)
+
+
 def contact(request):
-    data = {
-        'title': 'Sabadiaz Jewelry - Contact Us',
-        'option': 'contact'
-    }
+    data = {'title': 'Sabadiaz Jewelry - Contact Us'}
+    addUserData(request, data)
+    data['option'] = 'contact'
+    data['current_page'] = 'Contact'
     return render(request, 'site/contact.html', data)
 
 
 def wishlist(request):
-    data = {
-        'title': 'Sabadiaz Jewelry - WishList',
-        'option': 'wishlist'
-    }
+    data = {'title': 'Sabadiaz Jewelry - WishList'}
+    addUserData(request, data)
+    data['option'] = 'wishlist'
+    data['current_page'] = 'Wishlist'
     return render(request, 'site/wishlist.html', data)
 
 
 def product_details(request, product_id):
-    product = Product.objects.get(id=product_id)
-    data = {
-        'title': 'Sabadiaz Jewelry - Product Details',
-        'option': 'product',
-        'product': product
-    }
+    data = {'title': 'Sabadiaz Jewelry - Product Details'}
+    addUserData(request, data)
+    data['option'] = 'product'
+    data['current_page'] = 'Product Details'
+    data['product'] = Product.objects.get(id=product_id)
     return render(request, 'site/product_details.html', data)
 
 
 # Common Views
 def login_user(request):
-    data = {
-        'title': 'Sabadiaz Jewelry - Log In',
-        'option': 'Login',
-        'user': request.user
-    }
+    data = {'title': 'Sabadiaz Jewelry - Log In'}
+    addUserData(request, data)
+    data['option'] = 'Login'
+
     if request.method == 'POST':
         try:
             user = authenticate(username=request.POST['email'].lower(), password=request.POST['password'])
@@ -89,10 +92,10 @@ def login_user(request):
 
 
 def register(request):
-    data = {
-        'title': 'Sabadiaz Jewelry - Register',
-        'option': 'Register'
-    }
+    data = {'title': 'Sabadiaz Jewelry - Register'}
+    addUserData(request, data)
+    data['option'] = 'Register'
+
     if request.method == 'POST':
         try:
             first_name = request.POST['first_name']
@@ -126,11 +129,10 @@ def register(request):
 
 
 def account(request):
-    data = {
-        'title': 'Sabadiaz Jewelry - My Account',
-        'option': 'My Account',
-        'user': request.user
-    }
+    data = {'title': 'Sabadiaz Jewelry - My Account'}
+    addUserData(request, data)
+    data['option'] = 'My Account'
+    data['current_page'] = 'My Account'
     return render(request, 'account.html', data)
 
 
